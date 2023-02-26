b6bc64444620eca42ffb147ed94d685de30d980c
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-02-26 07:47:29 +0000

    Remove Test

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const DbName = process.env.DATABASE_NAME;
 
 router.get("/test", (req, res) => {
-  return res.send("<p>test</p>");
+  return res.send(DbName);
 });
 
 module.exports = router;
